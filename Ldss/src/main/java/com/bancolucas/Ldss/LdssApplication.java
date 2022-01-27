87e24982ee3d1f77ab51c294fb1608a492d8a001
Lucas Da Silva Santos <lsisanto@emeal.nttdata.com>
2022-01-26 23:07:23 -0300

    Create CRUD Des2

---
+++ Ldss/src/main/java/com/bancolucas/Ldss/LdssApplication.java
@@ -0,0 +1,13 @@
+package com.bancolucas.Ldss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdssApplication.class, args);
+	}
+
+}
---
+++ Ldss/src/test/java/com/bancolucas/Ldss/LdssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolucas.Ldss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
